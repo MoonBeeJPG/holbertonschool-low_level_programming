61c1db6c3ab7e0d7128b636d38e2d6f6480667fd
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-03 11:42:03 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -6,17 +6,7 @@
    * Return: Always 0 (Success)
     */
 	int main(void)
-	{  char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-	long l = 14; 
-	long fd = 1; 
-	long syscall = 1; 
-	long ret = 0; 
-	 __asm__ ( "syscall" 
-		: "=a" (ret) 
-		: "a" (syscall), 
-		"D" (fd), 
-		"S" (s), 
-		"d" (l) 
-	); 
-		    return (0);
+	{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19)\n", 59);
+	return (1);
 	}

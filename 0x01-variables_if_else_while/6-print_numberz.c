4eeafad4a2b4bea12d3f67e17837547644346ba3
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-04 11:52:54 -0800

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry Code
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 48 ; letter <= 57 ; letter++)
+	{
+			putchar(letter);
+	}
+		  putchar('\n');
+			return (0);
+}

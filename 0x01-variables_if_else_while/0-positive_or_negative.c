aae5872db50f230ed1b1e3d124eb4489f37eec53
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-04 04:53:37 -0800

    commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry Point
+*
+* Return: Almays 0 (Sucess)
+*
+*/
+
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					while (n = 0)
+					{
+						n = 0;
+					}
+					if (n > 0)
+					{
+						n > 0;
+					}
+					else
+						(n < 0);
+					{
+						n < 0;
+					}
+
+					return (0);
+}

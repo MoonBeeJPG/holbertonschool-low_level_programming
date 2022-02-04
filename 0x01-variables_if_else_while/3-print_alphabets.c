04a45e7ebb08a85f3df16f468f204240d83eaed9
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-04 10:14:12 -0800

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry Code
+*
+* Return: Always 0 (Success)
+*
+*/
 
+int main(void)
+{
+	char letter;
+	char ch;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (ch = 'A' ; ch <= 'Z' ; letter++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}

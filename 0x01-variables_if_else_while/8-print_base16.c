59e069fdb7c2d6f66085b5fa265183bd19283dd5
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-04 12:10:27 -0800

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry Code
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 48 ; letter <= 57 ; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'a' ; letter <= 'f' ; letter++)
+	{
+		putchar(letter);
+	}
+putchar('\n');
+return (0);
+}

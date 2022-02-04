1d433da6c168996301dce5a61c7602621375a378
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-04 10:59:26 -0800

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry Code
+*
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = ('a') ; letter <= ('z') ; letter++)
+	{
+		putchar(letter);
+	}
+
+	if (letter != 101 && letter != 103)
+		putchar(letter);
+	      putchar('\n');
+return (0);
+}

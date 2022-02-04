2b191651a47908a303eab2263fc55dc322961981
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-04 08:07:39 -0800

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,17 +15,17 @@ int main(void)
 
 			srand(time(0));
 			  n = rand() - RAND_MAX / 2;
-					while (n = 0)
+					if (n == 0)
 					{
-						printf("$n is zero");
-					}
-					if (n > 0)
+						printf("%d is zero\n", n);
+				}
+					else if (n > 0)
 					{
-						printf("$n is positive");
+						printf("%d is positive\n", n);
 						}
-					else if (n < 0)
+					else
 					{
-						printf("$n is negative");
+						printf("%d is negative\n", n);
 					}
 
 					return (0);
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Sucess)
+*
+*/
+int Last_Digit|
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n = 0)
+	{
+		printf("Last digit of %d is 0 and is 0 \n", n);
+		}
+	else if (n > 0)
+	{
+		printf("Last digit of %D is grather than 5 ", n);
+		}
+	else
+	{
+		}
+	return (0);
+}

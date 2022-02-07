d63bf986cf9379331b1a66fb4a09f9528e519a7a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-07 11:41:57 -0800

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,18 @@
 #include <ctype.h>
 #include "main.h"
 /**
+* Return: 1 if is lowercase, 0 if is upcase
+* @c: fe
 * _islower - chiquistrikis
 */
 int _islower(int c)
 {
-if (c <= 'a' && 'z' => c)
-{
+if (c >= 'a' && c <= 'z')
+
 return (1);
-}
+
 else
-{
+
 return (0);
-}
+
 }

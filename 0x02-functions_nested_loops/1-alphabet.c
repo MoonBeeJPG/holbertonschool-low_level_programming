7f5471eae789f466b8aae221207feca0e5decc26
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-07 10:28:36 -0800

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include "main.h"
 /**
-* main - check the code
-*
-* Return: Always 0.
+* print_alphabet - hola
 */
-int main(void)
+void print_alphabet(void)
 {
-	char letter;
-letter = 'a';
-	while (letter <= 'z')
-	{
-		void print_alphabet(void);
-	}
-putchar('\n');
-return (0);
+	char c;
+for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+_putchar('\n');
 }

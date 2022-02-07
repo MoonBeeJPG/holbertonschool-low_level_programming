4a8318a001342fd3756525e249b18cb348b496b9
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-07 10:44:59 -0800

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,19 @@
 #include "main.h"
 /**
-* print_alphabet - hola
+* print_alphabet_x10 - hola
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-int i;
-for (i = 1; i <= 5; ++i)
+int letter, count;
+	count = 0;
 
+while (count < 10)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	_putchar(letter);
+}
+count++;
 _putchar('\n');
+}
 }

c837db30096c8361ed9daa5fa49d34a069562833
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-07 08:42:15 -0800

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,18 @@
 *
 * Return: Always 0.
 */
+void print_alphabet_x10(void)
+{
+  char letter;
+
+  letter = 'a';
+  while (letter <= 'z')
+  {
+    putchar(letter);
+    letter++;
+  }
+}
+
 void print_alphabet(void)
 {
   char letter;

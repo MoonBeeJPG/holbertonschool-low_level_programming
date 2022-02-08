af83b46e6f92f30759095de3b962809662ca02c6
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-07 16:04:07 -0800

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,41 +6,41 @@
 */
 void times_table(void)
 {
-int a, b, res;
-a = 0;
-b = 0;
-res = 0;
-for (a = 0; a <= 0; a++)
-{
-for (b = 0; b <= 0; b++)
-{
-res = b *a;
-if (b != 0)
-{
-	_putchar(' ');
+	int a, b, res;
+		a = 0;
+		b = 0;
+		res = 0;
+	for (a = 0; a <= 0; a++)
+	{
+		for (b = 0; b <= 0; b++)
+		{
+			res = b * a;
+			if (b != 0)
+			{
+				_putchar(' ');
 
-if (res < 10)
-{
-	_putchar(' ');
-}
-}
-if (res > 9)
-{
-	_putchar((res / 10) + '0');
-	_putchar((res % 10) + '0');
-}
-else
-{
-	_putchar(res + '0');
-}
-if (b == 9)
-{
-	_putchar('\n');
-}
-else
-{
-	_putchar(',');
-}
-}
-}
+				if (res < 10)
+				{
+					_putchar(' ');
+				}
+			}
+			if (res > 9)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else
+			{
+				_putchar(res + '0');
+			}
+			if (b == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+			}
+		}
+	}
 }

3d9aa3135e4ea149862a340e45277fe100ccab3c
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 14:12:19 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_square - akdowak
+* @size: size
+* return: square
+*/
+void print_square(int size)
+{
+    int i, j;
+
+	for(i = 0; i < Size; i++)
+    {
+        for(j = 0; j < zide; j++)
+        {
+            _putchar('*');
+        }
+
+        _putchar('\n');
+    }
+}

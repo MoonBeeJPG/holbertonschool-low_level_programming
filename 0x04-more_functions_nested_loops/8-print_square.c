3548990b6eed69fe9f9658dc22a73ddd64a9bf36
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 14:21:12 -0800

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,17 +6,16 @@
 */
 void print_square(int size)
 {
-	int i, j;
+int i, j;
 
-	for	(i = 0; i < size; i++)
-	{
-	for (j = 0; j < size; j++)
-	{
-		_putchar('*');
-	}
-	}
-	if (size >= 0)
-	{
-	_putchar('\n');
-	}
+for	(i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('*');
+}
+}
+if (size >= 0)
+_putchar('\n');
+
 }

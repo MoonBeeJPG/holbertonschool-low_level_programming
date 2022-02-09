46512a63b7d9e538f0efd4a7c3fa3577cad1eb41
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 10:21:16 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* more_numbers -  a function that prints 10 times the numbers
+* Return: from 0 to 14
+*/
+void more_numbers(void)
+{
+	int num, count, a;
+count = 0;
+	a = 0;
+	for (num = 0; num <= a; num++)
+	{
+		while (count < 10)
+		{
+			_putchar(num);
+			num++;
+		}
+	}
+	_putchar('\n');
+}

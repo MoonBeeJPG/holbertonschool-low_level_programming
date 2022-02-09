fdade33cb17ea2b1b1ef20b3b41b202dc7faf2e8
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 12:30:56 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_diagonal - a function that draws a diagonal line
+* @n: variable
+* Return: Always 0.
+*/
+void print_diagonal(int n)
+{
+	int count, s, x;
+
+	if (n >= 1)
+	{
+		for (count = 1; count < n; count++)
+			_putchar('')
+			_putchar('\\');
+			_putchar('\n');
+	}
+	if (n >= 0)
+	_putchar('\n');
+}

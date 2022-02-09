c508ef2f0146e99824193a160bb175a717a021b1
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 14:04:12 -0800

    commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+int main(void);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,7 +11,8 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-
 void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif /* MAIN_H */

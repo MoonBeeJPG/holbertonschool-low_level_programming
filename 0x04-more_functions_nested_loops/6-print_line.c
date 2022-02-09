e29aa8cd5d89f4a7413534c0215397605ae687b5
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 11:28:31 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_line:  a function that draws a straight line
+* @n: number
+* @count: counter
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+int count;
+if (n >= 1)
+{
+for (count = 0; count <= n; count++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}

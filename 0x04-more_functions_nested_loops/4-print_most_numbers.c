54fd31ed87832d99940717e1c4e2f740e7d5e2d2
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 09:42:22 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_most_numbers - a function that prints the numbers 0 to 9
+* Return: numbers 0 to 9
+*/
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		while (c != 54 && c != 50)
+			continue;
+		_putchar(c);
+	}
+	_putchar('\n');
+}

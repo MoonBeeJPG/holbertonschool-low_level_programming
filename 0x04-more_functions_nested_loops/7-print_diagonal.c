caaa06c68685384823fd035f24a98766393257f1
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 12:39:18 -0800

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
-* print_diagonal - a function that draws a diagonal line
-* @n: variable
-* Return: Always 0.
+ *print_diagonal - yuk
+ *@n: sdfx
+ *
+ * Return:fghj
 */
 void print_diagonal(int n)
 {
-int x, e;
+	int x, e;
 
-if (n >= 0)
-{
-for (x = 0; x < n; x++)
-{
-for (e = 0; e < x; e++)
-_putchar(' ');
-_putchar('\\');
-if (x == n - 1)
-continue;
+	if (n >= 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (e = 0; e < x; e++)
+				_putchar(' ');
+			_putchar('\\');
+			if (x == n - 1)
+				continue;
 
-_putchar('\n');
+			_putchar('\n');
 
-}
-}
-_putchar('\n');
+		}
+	}
+	_putchar('\n');
 }

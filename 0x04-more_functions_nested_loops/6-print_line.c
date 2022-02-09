499e3f2be5a75c2e4007253d86c6d69ada4eb18e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-09 11:36:04 -0800

    commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,7 @@ void print_line(int n)
 {
 int count;
 
-if (n >= 1)
+if (n > 0)
 {
 for (count = 0; count <= n; count++)
 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 
-void print_line(int n);
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,5 +9,6 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 #endif /* MAIN_H */

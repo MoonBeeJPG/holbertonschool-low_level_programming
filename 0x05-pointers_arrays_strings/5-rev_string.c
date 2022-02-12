ff0103e04727253dedf5205df14bc016289861c5
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-11 17:07:06 -0800

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,25 @@
 #include "main.h"
-
 /**
- * rev_string - a function that reverses a string
+ *rev_string - asd
+ *@s: asd
  *
- * @s: daed
  */
 void rev_string(char *s)
 {
 	int pos = 0;
-    int i = 0;
+	int i = 0;
+	char sup;
 
-    while (*(s + 1) != '\0')
-        pos++;
+	while (s[pos] != '\0')
+	{
+		pos++;
+		sup++;
+	}
 
-    for (;i = pos - 1; i >= 0)
-		i--;
-}	
+	for (i = pos - 1; i >= pos / 2; i--)
+	{
+		sup = s[i];
+		s[i] = s[pos - i - 1];
+		s[pos - i - 1] = sup;
+	}
+}

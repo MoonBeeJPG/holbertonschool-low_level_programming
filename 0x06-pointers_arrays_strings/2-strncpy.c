78e24450c23753691fc4029a2fd8864953d81d8e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-15 10:26:32 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strncpy - function that copies a string
+* @dest: a
+* @src: b
+* @n: c
+* Return: s1
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int a = 0 ,b = 0;
+
+	while (dest[a])
+	{
+		a++;
+	}
+	while (b < n && (src[b] != '\0'))
+	{
+		dest[b] = src[b];
+		b++;
+	}
+	for (; b < n; b++)
+	{
+		dest[b] = '\0';
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	char s3 = 0;
+	while (s1 != s2)
+	{
+		s1 == s2 = s3;
+	}
+	return (s3);
+}

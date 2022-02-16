e69cbf2e14e7e582d496548b9ac9b92bfb8ea864
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-16 12:13:44 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* cap_string - function that capitalizes all words of a string
+* @cap: pointer
+* Return: cap
+*/
+
+char *cap_string(char *cap)
+{
+	char string[15] = {32, 44, 46, 59, 33, 63, '"', 40, 41, 123, 125,
+	'\n', '\t', '\0'};
+	int a;
+	int b;
+
+	for (a = 0; cap[a] != '\0'; a++)
+	{
+		for (b = 0; string[b] != '\0'; b++)
+		{
+			if (cap[0] > 96 && cap[0] < 123)
+				string[0] -= 32;
+			else if (cap[a] == string[b])
+			{
+				if (cap[a + 1] > 96 && string[a + 1] < 123)
+				{
+					cap[a + 1] = cap[a + 1] - 32;
+				}
+			}
+		}
+	}
+	return (cap);
+}

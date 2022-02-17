97ac5007dd2f2a0004d624d78f1c0479bae833bb
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-17 05:20:01 -0800

    commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,8 +7,8 @@
 
 char *rot13(char *a)
 {
-	char abc[53] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\0";
-	char rot[53] = "NOPQRSTUVWXYZnopqrstuvwxyz\0";
+	char abc[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\0";
+	char rot[] = "NOPQRSTUVWXYZnopqrstuvwxyz\0";
 	int x, z;
 
 	for (x = 0; a[x] != '\0'; x++)

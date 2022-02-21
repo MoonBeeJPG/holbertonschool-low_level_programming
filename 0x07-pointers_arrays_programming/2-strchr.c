a1c773896ff604c8a40560319ff318520959e1cb
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-21 07:36:05 -0800

    commit

---
+++ 0x07-pointers_arrays_programming/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strchr - a function that locates a character in a string
+* @s: a pointer
+* @c: a variable
+* Return: character c
+*/
+char *_strchr(char *s, char c)
+{
+	int a, b;
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		return (0);
+		break;
+	}
+	for (b = 0; s[b] != '\0'; b++)
+	return (s);
+}

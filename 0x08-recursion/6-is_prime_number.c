e8fc0f2f63ca48c652c479d9543a2c2b8fe68d4a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-23 14:14:11 -0800

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,10 +9,11 @@ int _calcu(int n, int x)
 {
 	if (n % x == 0)
 		return (0);
-	else if (n == 1)
+	else if (n <= 1)
 		return (0);
-	else
-		return (_calcu(n, n - 1));
+	else if (x == n)
+		return (1);
+	return (_calcu(n, n - 1));
 }
 /**
 * is_prime_number - returns 1 if the input integer is a prime number
@@ -21,8 +22,5 @@ int _calcu(int n, int x)
 */
 int is_prime_number(int n)
 {
-	if (n < 1)
-		return (0);
-
 	return (_calcu(n, n - 1));
 }

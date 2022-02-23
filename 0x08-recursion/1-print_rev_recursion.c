6acbaaf79be01f8de15807191c48060d03e25935
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-23 09:16:58 -0800

    commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,12 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-    {
-        return;
-    }
-    else
+	{
+		return;
+	}
+	else
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-    }
+	}
 }

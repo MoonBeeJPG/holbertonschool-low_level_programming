3ddac286511046d0776722e42791a635553ae6a0
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-23 06:28:37 -0800

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,15 @@
 * @s: a pointer
 */
 void _puts_recursion(char *s)
-{ 
-	if (s == '\0')
+{
+	if (*s == '\0')
 	{
 		_putchar('\n');
-		return (0);
+		return;
 	}
 	else
 	{
 		_putchar(*s);
-		_puts_recursions(s + 1);
+		_puts_recursion(s + 1);
 	}
 }

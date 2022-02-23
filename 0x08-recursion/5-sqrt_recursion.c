09ed528905cc6d03e8e3a44c63f687e3475774fa
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-23 13:05:29 -0800

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,17 +9,19 @@ int _calculator(int x, int n)
 {
 	if (x * x == n)
 		return (x);
-	else if (x * x > n)
+
+	if (x * x > n)
 		return (-1);
-	else
-		return (_calculator(n, x + 1));
+
+	return (_calculator(n, x + 1));
 }
+
 /**
-* _sqrt_recursion - a function that returns the natural square root of a number
-* @n: a variable
-* Return: natural square
-*/
+ * _sqrt_recursion - function that returns the natural square root of a number
+ *@n: a variable
+ * Return: a natural square root
+ */
 int _sqrt_recursion(int n)
 {
-	return (_calculator(0, n));
+	return (_calculator(n, 0));
 }

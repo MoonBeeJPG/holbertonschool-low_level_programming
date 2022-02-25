ec0c8dcee5963c807b428903fc4ebb144256c4f0
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-25 07:39:42 -0800

    commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-*
-*
-*
+* main - a program that prints the number of arguments passed into it
+* @argc: count
+* @argv: unused
+* Return: 0
 */
 int main(int argc, char *argv[] __attribute__ ((unused)))
 {
 	printf("%d\n", argc - 1);
 
-return 0;
+return (0);
 }

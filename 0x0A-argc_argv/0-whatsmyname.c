bbd81117248eef6bca293dbcc613847d36b6946f
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-02-25 07:38:37 -0800

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* whatsmyname - 
-* @argc: Number of arguments inside 
-* @argv: Value 
+* main -  a program that prints its name
+* @argc: Number of arguments inside
+* @argv: Value
 * Return: 0 (Success)
 */
 int main(int argc, char *argv[])
 {
-		printf("%s\n", argv[argc - 1]);
-	return 0;
+	printf("%s\n", argv[argc - 1]);
+	return (0);
 }
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+int main (int agrc; char *argv)
+{
+	int a, b, c = 0;
+
+	if (argc[a])
+		printf("Error\n");
+		return (1);
+	else if (argc[b])
+		printf("Error\n");
+		return (1);
+	else
+		for (; c < argc; c++)
+			c = atoi(argv[a]) * atoi(argv[b])
+		printf("%s\n", c)
+	return 0;
+}

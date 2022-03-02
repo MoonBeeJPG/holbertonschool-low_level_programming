83611ae65dd61185e785889d58ae5d79cb92d8ae
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-02 08:55:22 -0800

    commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* create_array - a function that creates an array of chars
+* @size: size
+* @c: variable
+* Return: Null if its null
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	char *pt;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	pt = malloc(size * sizeof(*pt));
+	if (pt == NULL)
+	{
+		return (NULL);
+	}
+	else
+	for (a = 0; a < size; a++)
+	{
+		pt[a] = c;
+	}
+	return (pt);
+}

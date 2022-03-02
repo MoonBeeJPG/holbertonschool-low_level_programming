947292854fa682c099288eeff2a7616547b8dcbe
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-02 10:11:13 -0800

    commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "main.h"
 /**
-* _strdup - a function that returns a pointer to a newly allocated space in memory
+* _strdup - a function that returns a pointer to a llocated space in memory
 * _strlen - to know the lenght of str
 * @str: contains a string
 * Return: Returns a pointer with the copy of str
@@ -11,20 +11,21 @@ char *_strdup(char *str)
 {
 	char *str2;
 	int i;
-	
-	if (*str == 0)
+
+	if (!str)
 	{
 		return (NULL);
 	}
 	str2 = malloc(strlen(str) + 1);
+	if (str2 == NULL)
+	{
+		return (NULL);
+	}
+	else
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		str2[i] = str[i];
 	}
 	str2 = '\0';
-	if (str2 ==	NULL)
-	{
-		return (NULL);
-	}
 	return (str2);
 }

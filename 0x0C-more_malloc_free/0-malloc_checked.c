e1e0ddfda99c3d3facd3447083fcc4dc4aef329f
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-03 05:31:14 -0800

    commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* malloc_checked - a function that allocates memory using malloc
+* @b: string
+* Return: pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b * sizeof(int *));
+	if (!ptr)
+	{
+		exit(98);
+	}
+	else
+	return (ptr);
+}

bddb87b4a1168f03a326c36297c4cfa8175979e0
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-03 06:49:59 -0800

    commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _strlen - calculate the lenght
+* @str: string
+* Return: string
+*/
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+/**
+* string_nconcat - a function that concatenates two strings
+* @s1: string one
+* @s2: string two
+* @n: a variable
+* Return: result
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *rslt;
+	unsigned int idx1, idx2;
+
+	rslt = malloc(_strlen(s1) + n);
+	if (!rslt)
+		return (NULL);
+	for (idx1 = 0; s1[idx1]; idx1++)
+		rslt[idx1] = s1[idx1];
+	for (idx2 = 0; idx2 < n && s2[idx2]; idx2++)
+		rslt[idx1 + idx2] = s2[idx2];
+	rslt[idx1 + idx2] = '\0';
+	return (rslt);
+}

93f7cebb18a8bab31381bac64cd829f9f5f8a5b9
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-03 07:27:36 -0800

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *rslt;
 	unsigned int idx1, idx2;
-
+	
+	if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
+		s2 = "";
 	rslt = malloc(_strlen(s1) + n * sizeof(*rslt));
 	if (!rslt)
 		return (NULL);

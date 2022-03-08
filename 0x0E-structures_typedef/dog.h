2cd97fd99a96fbe531c11e446c65e9970b97157a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-08 14:23:32 -0800

    Poppy!

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef TYPES_H
+#define TYPES_H
+#include <stdio.h>
+
+/**
+* struct dog - Doggie!
+* @name: Poppy
+* @age: 3.5
+* @owner: Bob
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif

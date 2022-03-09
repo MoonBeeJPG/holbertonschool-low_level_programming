d4e905526216599facf9a1b7231c73319f7b149c
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-09 15:50:23 -0800

    Betty

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,26 +1,28 @@
 #include "3-calc.h"
 
 /**
-* @get_op_func - contain the function that selects the correct 
+* get_op_func - contain the function that selects the correct
 * function to perform the operation asked
 * @s: argument to the program
 * Return: Null
 */
 int (*get_op_func(char *s))(int, int)
-{	
+{
 	op_t ops[] = {
-        	{"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
-        	{NULL, NULL}
-    };
-    int i;	
-	while (ops[i] != NULL)
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[0].op != NULL)
 	{
-		if (s == ops[i])
-			return (ops[i].f)
+		if (s[0] == ops[i].op[0])
+			return (ops[i].f);
+		i++;
 	}
 return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,61 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
+
 /**
 * op_add - program that performs addition
 * @a: num1
 * @b: num2
 * Return: Addition
 */
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
+
 /**
 * op_sub - program that performs subtraction
 * @a: num1
 * @b: num2
 * Return: Subtraction
 */
-int op_sub(int a, int b);
+int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
+
 /**
 * op_mul - program that performs multiplication
 * @a: num1
 * @b: num2
 * Return: Multiplication
 */
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
+
 /**
 * op_div - program that performs division
 * @a: num1
 * @b: num2
 * Return: Division
 */
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
-	if (b == '0')
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a / b);
+if (b == '0')
+{
+printf("Error\n");
+exit(100);
 }
+return (a / b);
+}
+
 /**
 * op_mod - program that performs modulo
 * @a: num1
 * @b: num2
 * Return: Modulo
 */
-int op_mod(int a, int b);
+int op_mod(int a, int b)
+if (b == '0')
 {
-	if (b == '0')
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }

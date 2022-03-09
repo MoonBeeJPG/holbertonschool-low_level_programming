0a02dc0a39b8c4a8b254b83c7e973c3cfb6d3443
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-09 07:48:43 -0800

    prueba

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+* int_indez - a function that searches for an integer
+* @array: an array
+* @size: size of the array
+* @cmp: pointer to the function to be used to compare values
+* Return: Null
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			cmp(array[i]);
+	return (i);
+}

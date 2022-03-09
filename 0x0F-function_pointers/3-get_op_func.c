a8f9447fb5128003502a2e51b3f60f15e2d3d303
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-09 09:09:22 -0800

    op_functions Done

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,14 @@
+#include "3-calc.h"
+
+/**
+* @get_op_func - contain the function that selects the correct 
+* function to perform the operation asked
+* @s: argument to the program
+* Return: Null
+*/
+int (*get_op_func(char *s))(int, int)
+{
+
+	if (s != ops[])
+		return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+* op_add - program that performs addition
+* @a: num1
+* @b: num2
+* Return: Addition
+*/
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+/**
+* op_sub - program that performs subtraction
+* @a: num1
+* @b: num2
+* Return: Subtraction
+*/
+int op_sub(int a, int b);
+{
+	return (a - b);
+}
+/**
+* op_mul - program that performs multiplication
+* @a: num1
+* @b: num2
+* Return: Multiplication
+*/
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+* op_div - program that performs division
+* @a: num1
+* @b: num2
+* Return: Division
+*/
+int op_div(int a, int b);
+{
+	return (a / b);
+}
+/**
+* op_mod - program that performs modulo
+* @a: num1
+* @b: num2
+* Return: Modulo
+*/
+int op_mod(int a, int b);
+{
+	return (a % b);
+}

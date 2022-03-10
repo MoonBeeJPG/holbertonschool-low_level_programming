82aa12e34af8a98848217c01ee41bd161c5a94ca
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-10 07:59:34 -0800

    Try1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *string;
 	string = va_arg(nlst, char *);
 
-	if (!string)
+	if (string == NULL)
 		printf("nil");
 	va_start(nlst, n);
 	for (i = 0; i < n;)

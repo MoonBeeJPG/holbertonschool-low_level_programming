bdb5db6af562c25b701294100f2a61ae1134fbc3
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-18 05:46:41 -0700

    Trying how to repair it x2

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,13 @@ void free_list(list_t *head)
 {
 	list_t *current, *nextnode;
 	
-	current = head->next;
+	current = head;
 	
 	if (current != NULL)
 	{
 		nextnode = current->next;
 		free(current->str);
 		free(current);
-		current->next = nextnode;
+		current = nextnode;
 	}
 }

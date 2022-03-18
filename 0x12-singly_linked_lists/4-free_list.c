bec3ec62a42e1b2a4dafc7f6ab4048fbfe0d653a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-18 05:40:09 -0700

    checking checks

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,12 +7,13 @@ void free_list(list_t *head)
 {
 	list_t *current, *nextnode;
 	
-	current = *head;
+	current = head;
+	
 	if (current != NULL)
 	{
 		nextnode = current->next;
 		free(current->str);
 		free(current);
-		current = nextnode;
+		current->next = nextnode;
 	}
 }

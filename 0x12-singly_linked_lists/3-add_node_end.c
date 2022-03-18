269f1f32cf74f89bf45c6adf922817cfcbc31d90
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-18 05:25:51 -0700

    checking codes

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,23 +7,27 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *nxt_str, next_node;
+	list_t *str1, *next_node;
 
 	if (!str)
 		return (NULL);
-	nxt_str = malloc(sizeof(list_t));
-	if (!nxt_str)
+	str1 = malloc(sizeof(list_t));
+	
+	if (!str1)
 		return (NULL);
-	nxt_str = nxt_str->str;
-	nxt_str = nxtr_str->len;
-	nxt_str->next = NULL;
+	str1->str = strdup(str);
+	str1->len = strlen(str);
+	str1->next = NULL;
 
 	if (head != NULL)
-		next_node = head;
-		while (next_node->next)
+	{
+		next_node = *head;
+		if (next_node->next)
 			next_node = next_node->next;
 		next_node = str1;
-	*head = str1;
+	}
+	else
+		*head = str1;
 	return (str1);
 }
 

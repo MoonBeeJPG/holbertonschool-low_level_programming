71bab7d9b673c766b4f6707f45c642779b69e87e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-18 06:01:50 -0700

    main repair

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
 * snd_main - second main with a string
 */
-void snd_main(void)
+void b_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");

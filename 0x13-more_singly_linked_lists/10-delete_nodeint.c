2a09ca81bf1f318442e7aad1951592e7b7f36687
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-22 17:23:12 -0700

    betty check

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,9 +15,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	
+
 	current = *head;
-	
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (i < (index - 1))
 	{
 		current = current->next;
-		
+
 		if (current == NULL)
 			return (-1);
 		i++;
@@ -36,5 +36,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	nextnode = current->next;
 	current->next = nextnode->next;
 	free(nextnode);
-	return(1);
+	return (1);
 }

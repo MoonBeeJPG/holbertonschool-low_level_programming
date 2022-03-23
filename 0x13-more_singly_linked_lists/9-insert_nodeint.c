d44dfa3b6b93480c3346b7dbe441471dc3c05efd
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-22 17:02:36 -0700

    betty check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,12 +28,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (*head);
 	}
-	
+
 	while (idx > 1)
 	{
 		current = current->next;
 		idx--;
-		
+
 		if (current == NULL)
 		{
 			free(new);

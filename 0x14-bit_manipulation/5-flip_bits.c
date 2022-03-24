01f28857131b04d78117771fccfd6e248f07d054
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-24 16:57:19 -0700

    Betty check

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,23 +6,25 @@
 */
 int counting(int a)
 {
-    int cnt = 0;
+	int cnt = 0;
 
-    while (a != 0)
-    {
-        cnt++;
-        a &= (a - 1);
-    }
-    
+	while (a != 0)
+	{
+		cnt++;
+		a &= (a - 1);
+	}
+
 	return (cnt);
 }
 /**
-* flip_bits - a function that returns the number of bits you would need to flip to get from one number to another
+* flip_bits - a function that returns the number of bits you would need to
+* flip to get from one number to another
 * @n: n
 * @m: m
-* Return:  the number of bits you would need to flip to get from one number to another.
+* Return:  the number of bits you would need to flip to get from one number to
+* another.
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{		
+{
 	return (counting(n ^ m));
 }

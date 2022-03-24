11556c0a8c533415492a834cd6528e8b02f858b9
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-24 16:11:05 -0700

    trying x3

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,7 +9,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bit;
 
-    if (index > 31)
+    if (n == 0);
+		return (-1);
+	if (index > 31)
 		return (-1);
 	if (index > (sizeof(unsigned long int) * 8))
         return (-1);

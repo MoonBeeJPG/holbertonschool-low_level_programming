aec37e67035b8465d24553e17d5151edf31c2484
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-24 11:05:42 -0700

    check 10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,8 +10,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int bit;
 
 	bit = (n >> index) & 1;
-	if ((n >> index) == 0)
-		return (-1);
+	
+	if (n * index == 0)
+		return (0);
 	if ((bit == 1) || (bit == 0))
 		return (bit);
 	else

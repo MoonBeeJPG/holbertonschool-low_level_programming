af700eb947a6b960f1ff72819067a776072fa674
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-24 10:00:28 -0700

    betty check

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,8 +2,8 @@
 /**
 * binary_to_uint - A function that converts a binary number to an unsigned int
 * @b: Is pointing to a string of 0 and 1 chars
-* Return: The converted number, or 0 if: there is one or more chars in the string b that is not 0 or 1
-* b is NULL
+* Return: The converted number, or 0 if: there is one or more chars in the
+* string b that is not 0 or 1 b is NULL
 */
 unsigned int binary_to_uint(const char *b)
 {
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 	while (b[i])
 	{
 		if (b[i] != '0' && b[i] != '1')

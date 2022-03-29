0addf08926741e86f35a8077fcf2a3228a66afa4
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-28 18:16:05 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,13 +9,12 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int op;
-	unsigned int i = 0;
+	int op, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	op = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
 	if (op == -1)
 		return (-1);

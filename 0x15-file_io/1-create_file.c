0eeb2fe8be7a341b371cb7ea156bd33fd8f8748a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-28 18:36:26 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+* _strlen - strlen
+* @string: string
+* Return: 0
+*/
+unsigned int _strlen(const char *string)
+{
+	unsigned int i = 0;
+
+	while (string[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
 * create_file - Create a function that creates a file
 * @filename: The name of the file to create
 * @text_content: A NULL terminated string to write to the file
@@ -9,7 +23,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, i = 0;
+	int op, wr;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,15 +33,12 @@ int create_file(const char *filename, char *text_content)
 	if (op == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
-
 	if (text_content == NULL)
 	{
 		close(op);
 		return (1);
 	}
-	wr = write(op, text_content, i);
+	wr = write(op, text_content, _strlen(text_content));
 
 	if (wr == -1)
 	{

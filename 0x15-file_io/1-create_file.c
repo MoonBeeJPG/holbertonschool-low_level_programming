45626555f463477970cd8d0d0cd2bf210995ce5e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-28 18:11:32 -0700

    checking

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int op, i = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	op = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);

e81bce440093208e273b41684ca6fdb7c9e0c7ea
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-03-28 18:25:54 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,5 +9,25 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int op, i = 0;
 
+    if (filename == NULL)
+        return (-1);
+
+    op = open(filename, O_CREAT | O_APPEND | O_RDWR, 0600);
+
+    if (op == -1)
+        return (-1);
+
+    while (text_content[i])
+        i++;
+
+    if (text_content == NULL)
+    {
+        return (1);
+    }
+    write(op, text_content, i);
+
+    close(op);
+    return (1);
 }

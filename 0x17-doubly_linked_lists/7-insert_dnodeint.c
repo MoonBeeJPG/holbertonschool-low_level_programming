e2914851aff11b4805f11767733f3c7efa284fc1
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-05 10:58:11 -0700

    checking task seven

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = current;
 		*h = new_node;
-		return (*h);
+		return (new_node);
 	}
 	while (idx > 1)
 	{

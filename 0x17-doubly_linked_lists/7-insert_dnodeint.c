c8ff880bf63f56dcc26ae23969fc82b61143f15b
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-05 11:52:16 -0700

    Trying to fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,6 +38,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
+	if (!current->next)
+		new_node->next = NULL;
 	new_node->next = current->next;
 	current->next->prev = new_node;
 	current->next = new_node;

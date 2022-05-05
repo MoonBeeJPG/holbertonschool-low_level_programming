95b1816b86c72045e4af6308e9da94b948a74bf7
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-05 09:24:27 -0700

    Betty check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-* add_nodeint - A function that adds a new node at the beggining
+* add_dnodeint - A function that adds a new node at the beggining
 * of a dlistint_t list
 *
 * @head: Head of the double linked list
@@ -13,9 +13,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	
+
 	if (!head)
-		return(NULL);
+		return (NULL);
 
 	if (!new_node)
 		return (NULL);
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->next = *head;
 	new_node->prev = NULL;
-	
+
 	if (*head != NULL)
 		(*head)->prev = new_node;
 

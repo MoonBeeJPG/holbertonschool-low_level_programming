073a91b080cc11a75c2d03d1a1ed962194f12ab7
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-05 10:31:21 -0700

    Betty check

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
 * @head: Header of the doubly linked list
 * @index: Index of the node
 *
-* Return: The nth node of the dlisint_t, if the node doesn't 
+* Return: The nth node of the dlisint_t, if the node doesn't
 * exist, NULL
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)

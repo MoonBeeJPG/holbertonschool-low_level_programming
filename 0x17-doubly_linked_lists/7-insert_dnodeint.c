1f9c2c9c85bda2fe16a10667bcebca48cd7a3b20
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-05 12:38:43 -0700

    Betty check

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (current == NULL)
 		{
 			free(new_node);
-			return (add_dnodeint_end(h, n));
+			return (add_dnodeint(h, n));
 		}
 	}
 	new_node->next = current->next;

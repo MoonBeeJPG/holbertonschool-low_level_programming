fa32e7bd59d5aa3f15c37b91a58af6903d481c34
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-05 11:40:02 -0700

    Trying to fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,13 +16,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
+	
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
 
-	if (idx < 1)
-		return (NULL);
 	if (idx == 0)
 	{
 		new_node->next = current;
@@ -41,6 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 	}
 	new_node->next = current->next;
+	current->next->prev = new_node;
 	current->next = new_node;
 	return (new_node);
 }

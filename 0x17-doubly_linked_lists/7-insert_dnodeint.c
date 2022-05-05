3b634670c74d1d5d620a43a9756508597fd50da8
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-05 11:42:49 -0700

    Trying to fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-	while (idx > 1)
+	while (idx >= 1)
 	{
 		current = current->next;
 		idx--;

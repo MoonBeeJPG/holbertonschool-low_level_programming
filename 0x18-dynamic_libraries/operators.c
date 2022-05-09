eb56ac9c587d57138d477f36dbe9a8baed72c7cc
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-09 10:25:17 -0700

    checking tast 100

---
+++ 0x18-dynamic_libraries/operators.c
@@ -0,0 +1,52 @@
+#include "operations.h"
+#include <stdio.h>
+/**
+* add - Function that makes the addition of two numbers
+* @a: Firt number passed
+* @b: Second number passed
+* Return: Result of the addition
+*/
+int add(int a, int b)
+{
+        return (a + b);
+}
+/**
+* sub - Function that makes the substraction of two numbers
+* @a: Firt number passed
+* @b: Second number passed
+* Return: Result of the substraction
+*/
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+* sub - Function that makes the multiplication of two numbers
+* @a: Firt number passed
+* @b: Second number passed
+* Return: Result of the multiplication
+*/
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+* sub - Function that makes the division of two numbers
+* @a: Firt number passed
+* @b: Second number passed
+* Return: Result of the division
+*/
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+* sub - Function that makes the module of two numbers
+* @a: Firt number passed
+* @b: Second number passed
+* Return: Result of the module
+*/
+int mod(int a, int b)
+{
+        return (a % b);
+}

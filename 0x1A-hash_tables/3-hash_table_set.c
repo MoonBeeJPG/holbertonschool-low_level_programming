3cde4fbff4f43897f8e63e0869bf2b9cd93b1343
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-27 16:04:56 -0700

    checking

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key == NULL || ht == NULL || ht->array == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	while (ht->array[index])
+	while (ht->array[index] != NULL)
 	{
 		if (strcmp(ht->array[index]->key, key) == 0)
 			break;

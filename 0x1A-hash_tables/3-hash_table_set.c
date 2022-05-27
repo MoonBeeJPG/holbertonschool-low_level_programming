2314008fae349c273844f3c362256bcfa78213c5
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-27 16:08:22 -0700

    checking

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,19 +9,19 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current = ht->array[index];
 	char *new_value;
 
 	if (key == NULL || ht == NULL || ht->array == NULL || *key == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	while (ht->array[index] != NULL)
+	while (current != NULL)
 	{
-		if (strcmp(ht->array[index]->key, key) == 0)
+		if (strcmp(current->key, key) == 0)
 			break;
-		ht->array[index] = ht->array[index]->next;
+		current = current->next;
 	}
-	if (ht->array[index] == NULL)
+	if (current == NULL)
 	{
 		new_node = malloc(sizeof(hash_node_t));
 		if (!new_node)
@@ -47,8 +47,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_value = strdup(value);
 		if (new_value == NULL)
 			return (0);
-		free(ht->array[index]->value);
-		ht->array[index]->value = new_value;
+		free(current->value);
+		current->value = new_value;
 	}
 	return (1);
 }

333a4df1f5bc957a877a5657c624731ef4856bbb
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-05-27 16:56:58 -0700

    checking betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,48 +8,34 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *current;
-	char *new_value;
+	unsigned long int index = 0;
+	hash_node_t *new_node = NULL, *current = NULL;
 
-	if (key == NULL || *key == 0 || ht == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
+
 	index = key_index((const unsigned char *) key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (!new_node)
+		return (0);
+
 	current = ht->array[index];
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
-			break;
+		{
+			free(current->value);
+			current->value = strdup(value);
+			free(new_node);
+			return (1);
+		}
 		current = current->next;
 	}
-	if (current == NULL)
-	{
-		new_node = malloc(sizeof(hash_node_t));
-		if (!new_node)
-			return (0);
-		new_node->key = strdup(key);
-		if (new_node->key == NULL)
-		{
-			free(new_node);
-			return (0);
-		}
-		new_node->value = strdup(value);
-		if (new_node->value == NULL)
-		{
-			free(new_node->key);
-			free(new_node);
-			return (0);
-		}
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		new_value = strdup(value);
-		if (!new_value)
-			return (0);
-		free(current->value);
-		current->value = new_value;
-	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
 	return (1);
 }

5faff71f724a17c92cf87ced70651f2aa2662be5
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-07-14 19:44:38 -0700

    Checking again

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,7 +8,7 @@ def island_perimeter(grid):
     perimeter = 0
 
     for x in range(len(grid)):
-        for y in range(len(grid)):
+        for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 if x == 0 or grid[x - 1][y] == 0:
                     perimeter += 1

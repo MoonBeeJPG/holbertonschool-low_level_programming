26c7378ed81d32a05ad124d4ffdfcf79bfb88eeb
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-07-14 19:41:43 -0700

    Checking one last time task five

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,11 +8,14 @@ def island_perimeter(grid):
     perimeter = 0
 
     for i in range(len(grid)):
-        for j in range(len(grid[i])):
+        for j in range(len(grid)):
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if i > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
     return perimeter

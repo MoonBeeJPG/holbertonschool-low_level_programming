0bed396c9d4f1109c6446a0369058c2c56794b8e
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-07-14 19:35:01 -0700

    Checking task five

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,8 +8,7 @@ def island_perimeter(grid):
     perimeter = 0
 
     for i in range(len(grid)):
-        j = 0
-        for j in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:

8bf9bcd9d41952ba7d8e60bb6ddc0cb549add47a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-07-14 19:46:36 -0700

    Checking again

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,15 +7,15 @@ def island_perimeter(grid):
     1 represents a land zone """
     perimeter = 0
 
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            if grid[x][y] == 1:
-                if x == 0 or grid[x - 1][y] == 0:
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if x == len(grid) - 1 or grid[x + 1][y] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if y == 0 or grid[x][y - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if y == len(grid[x]) - 1 or grid[x][y + 1] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
